b1ff668be2714cc59d9ad79af367507d94e44079
Vedant gupta <132246344+vedgupta999@users.noreply.github.com>
2025-08-10 12:32:37 +0530

    Week1

---
+++ Time Conversion.c
@@ -0,0 +1,104 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+char* timeConversion(char* s) {
+
+static char result[9];  
+    int hour;
+    char meridian[3];
+
+   
+    hour = (s[0] - '0') * 10 + (s[1] - '0');
+    meridian[0] = s[8];
+    meridian[1] = s[9];
+    meridian[2] = '\0'; 
+
+    if (strcmp(meridian, "AM") == 0) {
+        if (hour == 12) hour = 0;
+    } else {
+        if (hour != 12) hour += 12;
+    }
+
+   
+    snprintf(result, sizeof(result), "%02d:%c%c:%c%c", hour, s[3], s[4], s[6], s[7]);
+
+    return result;
+
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = timeConversion(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
